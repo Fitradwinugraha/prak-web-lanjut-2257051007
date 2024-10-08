47f4f3a4c77aad70b13d2a7dafa81a32813d2240
Fitradwinugraha <fitradwinugraha87@gmail.com>
2024-10-08 09:42:48 +0700

    modul 5 dan tugas

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -89,4 +89,60 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</html> -->
+
+
+
+@extends('layouts.app')
+ @section('content')
+ <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="col-md-6 form-box">
+        <form action="{{route('user.store')}}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <h2 class="text-center mb-4">Masukkan Data</h2>
+        
+       
+        <div class="mb-3">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama">
+            @foreach($errors->get('nama') as $msg)
+                <div class="error-message">{{ $msg }}</div>
+            @endforeach
+        </div>
+
+       
+        <div class="mb-3">
+            <label for="npm" class="form-label">NPM</label>
+            <input type="text" class="form-control" id="npm" name="npm" placeholder="Masukkan NPM" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
+            @foreach($errors->get('npm') as $msg)
+                <div class="error-message">{{ $msg }}</div>
+            @endforeach
+        </div>
+
+      
+        <div class="mb-3">
+            <label for="kelas" class="form-label">Kelas</label>
+            <select name="kelas_id" id="kelas" class="form-control">
+                <option value="" selected disabled>-- Pilih Kelas --</option>
+                @foreach($kelas as $kelasItem)
+                <option value="{{ $kelasItem->id }}">{{ $kelasItem->nama_kelas }}</option>
+                @endforeach
+            </select>
+            @foreach($errors->get('kelas_id') as $msg)
+                <div class="error-message">{{ $msg }}</div>
+            @endforeach
+        </div>
+        
+        <div class="mb-3">
+                <label for="foto" class="form-label">Foto</label>
+                <input type="file" class="form-control" id="foto" name="foto">
+                @foreach($errors->get('foto') as $msg)
+                    <div class="error-message">{{ $msg }}</div>
+                @endforeach
+            </div>
+       
+        <button type="submit" class="btn btn-custom w-100">Submit</button>
+    </form>
+    </div>
+</div> 
+@endsection
